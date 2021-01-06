df1f7f2ac15607142c8df31e7decc9f7253d737c
navneet <navneet3265@gmail.com>
2021-01-05 18:53:44 -0600

    refactor the solution

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,7 @@
-//navigator.geolocation.getcurrentPosition(success, error);
-//gives the long and latitude
-
-//returns current weather conditions
-//https://api.openweathermap.org/data/2.5/weather?lat=49.785175&lon=-97.169907&units=metric&appid=3dcadafeade3c14467c5e00cecb41843
-
-//https://api.openweathermap.org/data/2.5/forecast?lat=49.785175&lon=-97.169907&units=metric&appid=3dcadafeade3c14467c5e00cecb41843
-//gets weather forecast for next 5 days
-
 const apikey = "appid=3dcadafeade3c14467c5e00cecb41843";
 const baseUrl = "https://api.openweathermap.org/data/2.5/";
 const currentConditionsEle = document.querySelector('.current-conditions');
 const forecastEle = document.querySelector('.forecast');
-const geo = navigator.geolocation;
 const todaysDate = new Date();
 let weatherObj = {
   1:{
@@ -36,7 +26,8 @@ let weatherObj = {
   }
 };
 
-function getCurrentPosition() {
+function printWeatherForCurrentPosition() {
+  const geo = navigator.geolocation;
   geo.getCurrentPosition((currLocation)=> {
     findCurrentWeather(currLocation.coords.latitude, currLocation.coords.longitude);
     findWeatherForecast(currLocation.coords.latitude, currLocation.coords.longitude);
@@ -60,7 +51,7 @@ function printCurrentWeather(weatherResp) {
   `<h2>Current Conditions</h2>
   <img src="http://openweathermap.org/img/wn/${weatherResp.weather[0].icon}@2x.png" />
   <div class="current">
-    <div class="temp">${Math.floor(weatherResp.main.temp)}℃</div>
+    <div class="temp">${Math.round(weatherResp.main.temp)}℃</div>
     <div class="condition">${weatherResp.weather[0].description}</div>
   </div>`)
 }
@@ -71,7 +62,6 @@ function findWeatherForecast(lat,long) {
   .then((weatherResp) => {
     collectForecastedWeatherData(weatherResp);
     printForecastWeatherData();
-    
   })
 }
 
@@ -79,15 +69,15 @@ function getDaysDiff(d1, d2) {
   return Math.floor((Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate()) - Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate()) ) /(1000 * 60 * 60 * 24) );
 } 
 
-function collectForecastedWeatherData(weather) {
-  weather.list.forEach(weatherInfo=> {
-    const currentDate = new Date(weatherInfo.dt_txt)
-    const difference = getDaysDiff(currentDate, todaysDate)
+function collectForecastedWeatherData(weatherResp) {
+  weatherResp.list.forEach(weatherInfo=> {
+    const nextDate = new Date(weatherInfo.dt_txt);
+    const difference = getDaysDiff(nextDate, todaysDate)
     if(weatherObj[difference] !== undefined) {
-      weatherObj[difference]["day"] = currentDate.getDay();
-      weatherObj[difference]["low"].push(Math.floor(weatherInfo.main.temp_min))
-      weatherObj[difference]["high"].push(Math.floor(weatherInfo.main.temp_max))
-      if(currentDate.getHours() === 12) {
+      weatherObj[difference]["day"] = nextDate.toLocaleString('en-us', {weekday: 'long'});
+      weatherObj[difference]["low"].push(Math.round(weatherInfo.main.temp_min))
+      weatherObj[difference]["high"].push(Math.round(weatherInfo.main.temp_max))
+      if(nextDate.getHours() === 12) {
         weatherObj[difference]["icon"] = weatherInfo.weather[0].icon
         weatherObj[difference]["description"] = weatherInfo.weather[0].description
       }
@@ -98,41 +88,17 @@ function collectForecastedWeatherData(weather) {
 function printForecastWeatherData() {
   forecastEle.innerHTML = "";
   for(let weatherData in weatherObj) {
-    let day="";
     const data = weatherObj[weatherData];
-    switch (data.day) {
-      case 0:
-        day = "Sunday";
-        break;
-      case 1:
-        day = "Monday";
-        break;
-      case 2:
-         day = "Tuesday";
-        break;
-      case 3:
-        day = "Wednesday";
-        break;
-      case 4:
-        day = "Thursday";
-        break;
-      case 5:
-        day = "Friday";
-        break;
-      case 6:
-        day = "Saturday";
-    }
     forecastEle.insertAdjacentHTML('beforeend', 
     `<div class="day">
-    <h3>${day}</h3>
+    <h3>${data.day}</h3>
     <img src="http://openweathermap.org/img/wn/${data.icon}@2x.png" />
     <div class="description">${data.description}</div>
     <div class="temp">
       <span class="high">${Math.max(...data["high"])}℃</span>/<span class="low">${Math.min(...data["low"])}℃</span>
     </div>
   </div>`)
-
   }
 }
 
-getCurrentPosition();
+printWeatherForCurrentPosition();
